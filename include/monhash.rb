47116ac39a95a04bcbf12db8e8708a00509b0514
Kasper Johansen <k@spernj.org>
2012-07-25 16:41:35 +0200

    Test with documentation for RubyGems (fucks up at the moment). Adjusted variable names to reflects classes.

--- include/monhash.rb
+++ include/monhash.rb
@@ -5,8 +5,8 @@
 # h['test'] = 'trala'
 # ret = h['test']
 class Tsafe::MonHash < ::Hash
-  @@tsafe_rwmutex_w_methods = [:[]=, :clear, :delete, :delete_if, :keep_if, :merge!, :rehash, :reject!, :replace, :select!, :shift, :store, :update, :values_at]
-  @@tsafe_rwmutex_r_methods = [:each, :each_key, :each_pair, :each_value]
+  @@tsafe_mrswlock_w_methods = [:[]=, :clear, :delete, :delete_if, :keep_if, :merge!, :rehash, :reject!, :replace, :select!, :shift, :store, :update, :values_at]
+  @@tsafe_mrswlock_r_methods = [:each, :each_key, :each_pair, :each_value]
   
   include Tsafe::Mrswlock::SynModule
 end
--- include/mrswlock.rb
+++ include/mrswlock.rb
@@ -2,13 +2,13 @@
 class Tsafe::Mrswlock
   @@debug = false
   
-  #Sets various variables.
+  # Sets various variables.
   def initialize
     @reads = 0
     @w_mutex = Mutex.new
   end
   
-  #Runs the given block through the read-synchronization.
+  # Runs the given block through the read-synchronization.
   def rsync
     begin
       while @w_mutex.locked?
@@ -24,7 +24,11 @@ class Tsafe::Mrswlock
     end
   end
   
-  #Runs the given block through the write-synchronization (locks both reading and writing).
+  # Runs the given block through the write-synchronization (locks both reading and writing).
+  # ===Examples
+  #  lock.wsync do
+  #    #do something within lock.
+  #  end
   def wsync
     @w_mutex.synchronize do
       #Wait for any reads to finish that might have started while we were getting the lock.
@@ -37,30 +41,46 @@ class Tsafe::Mrswlock
     end
   end
   
+  #This module can be included in order to painlessly make a thread-safe multi-reader-single-writer thread-safe copy of a class.
+  #===Examples
+  # class Tsafe::MonHash < ::Hash
+  #   @@tsafe_mrswlock_w_methods = [:[]=, :clear, :delete, :delete_if, :keep_if, :merge!, :rehash, :reject!, :replace, :select!, :shift, :store, :update, :values_at]
+  #   @@tsafe_mrswlock_r_methods = [:each, :each_key, :each_pair, :each_value]
+  #   include Tsafe::Mrswlock::SynModule
+  # end
   module SynModule
     def self.included(base)
       base.to_s.split("::").inject(Object, :const_get).class_eval do
         #Yields the given block within the read-lock.
+        #===Examples
+        # obj._tsafe_rsync do
+        #   #do something within read-lock.
+        # end
         def _tsafe_rsync(&block)
           @tsafe_mrswlock.rsync(&block)
         end
         
         #Yields the given block within the write-lock (and read-lock).
+        #===Examples
+        # obj._tsafe_wsync do
+        #   #do something within write-lock.
+        # end
         def _tsafe_wsync(&block)
           @tsafe_mrswlock.rsync(&block)
         end
         
         #Rename initialize.
-        alias_method(:initialize_rwmutex, :initialize)
+        alias_method(:initialize_mrswlock, :initialize)
         
         #Make another initialize-method that spawns the lock and then calls the original initialize.
         define_method(:initialize) do |*args, &block|
           @tsafe_mrswlock = Tsafe::Mrswlock.new
-          return initialize_rwmutex(*args, &block)
+          return initialize_mrswlock(*args, &block)
         end
         
-        base.class_variable_get(:@@tsafe_rwmutex_r_methods).each do |mname|
-          newmname = "tsafe_rwmutex_#{mname}".to_sym
+        #Makes reader methods go through reader-lock.
+        base.class_variable_get(:@@tsafe_mrswlock_r_methods).each do |mname|
+          newmname = "tsafe_mrswlock_#{mname}".to_sym
           alias_method(newmname, mname)
           
           define_method(mname) do |*args, &block|
@@ -70,8 +90,9 @@ class Tsafe::Mrswlock
           end
         end
         
-        base.class_variable_get(:@@tsafe_rwmutex_w_methods).each do |mname|
-          newmname = "tsafe_rwmutex_#{mname}".to_sym
+        #Makes writer methods go through writer-lock.
+        base.class_variable_get(:@@tsafe_mrswlock_w_methods).each do |mname|
+          newmname = "tsafe_mrswlock_#{mname}".to_sym
           alias_method(newmname, mname)
           
           define_method(mname) do |*args, &block|
